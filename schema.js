77f850ccf9ce5ea9a94be615cd73b68fd3b52a96
xBasox <borko12329@gmail.com>
2022-07-09 16:42:57 +0300

    set up server.js

--- schema.js
+++ schema.js
@@ -1,9 +1,12 @@
+const axios = require("axios");
+
 const {
   GraphQLObjectType,
   GraphQLInt,
   GraphQLString,
   GraphQLBoolean,
   GraphQLList,
+  GraphQLSchema,
 } = require("graphql");
 
 // Launch Type
@@ -36,8 +39,14 @@ const RootQuery = new GraphQLObjectType({
     launches: {
       type: new GraphQLList(LaunchType),
       resolve(parent, args) {
-        
-      }
+        return axios
+          .get("https://api.spacexdata.com/v5/launches")
+          .then((res = res.data));
+      },
     },
   },
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
 });
