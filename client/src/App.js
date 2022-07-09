36354dfdac3e49ba93d340c052b1623d85b42a22
xBasox <borko12329@gmail.com>
2022-07-09 20:11:04 +0300

    adding cyborg theme

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import "./bootswatch/dist/cyborg/bootstrap.min.css";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>SpaceX</h1>
+    </div>
+  );
+}
+
+export default App;
