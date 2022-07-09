2adb9bdb6d76ec2dfb77eb8e1505bcabc0d5502b
xBasox <borko12329@gmail.com>
2022-07-09 18:55:07 +0300

    graphql server created

--- schema.js
+++ schema.js
@@ -56,6 +56,25 @@ const RootQuery = new GraphQLObjectType({
           .then((res) => res.data);
       },
     },
+    rockets: {
+      type: new GraphQLList(RocketType),
+      resolve() {
+        return axios
+          .get("https://api.spacexdata.com/v3/rockets")
+          .then((res) => res.data);
+      },
+    },
+    rocket: {
+      type: RocketType,
+      args: {
+        id: { type: GraphQLString },
+      },
+      resolve(parent, args) {
+        return axios
+          .get(`https://api.spacexdata.com/v3/rockets/${args.id}`)
+          .then((res) => res.data);
+      },
+    },
   },
 });
 
