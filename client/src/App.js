34d0745582ec4ae713e6a5b3eb611223b113bb30
xBasox <borko12329@gmail.com>
2022-07-09 22:57:57 +0300

    restructuring code for older but working version of modules

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,20 @@
-import "./bootswatch/dist/cyborg/bootstrap.min.css";
-import './App.css';
+import "../node_modules/bootswatch/dist/cyborg/bootstrap.min.css";
+import "./App.css";
+import React from "react";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+
+const client = new ApolloClient({
+  uri: "https://localhost:5000/graphql",
+});
 
 function App() {
   return (
-    <div className="App">
-      <h1>SpaceX</h1>
-    </div>
+    <ApolloProvider client={client}>
+      <div className="container">
+        <h1>SpaceX</h1>
+      </div>
+    </ApolloProvider>
   );
 }
 
