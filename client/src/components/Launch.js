bfb6f9453b605c1029975f4080ad5ce356fe4a55
xBasox <borko12329@gmail.com>
2022-07-11 18:55:18 +0300

    getting ready for finishing touches

--- client/src/components/Launch.js
+++ client/src/components/Launch.js
@@ -39,15 +39,52 @@ export class Launch extends Component {
               flight_number,
               launch_year,
               launch_success,
+              details,
               rocket: { rocket_id, rocket_name, rocket_type },
             } = data.launch;
 
             return (
-              <div>
+              <>
                 <h1 className="display-4 my-3">
                   <span className="text-white">Mission: </span> {mission_name}
                 </h1>
-              </div>
+                <h4 className="mb-3">Short Details</h4>
+                <ul className="list-group">
+                  <li className="list-group-item">
+                    Flight Number: {flight_number}
+                  </li>
+                  <li className="list-group-item">
+                    Launch Year: {launch_year}
+                  </li>
+                  <li className="list-group-item">
+                    Launch Successful:{" "}
+                    {launch_success ? (
+                      <span className="text-success">YES</span>
+                    ) : (
+                      <span className="text-danger">NO!</span>
+                    )}
+                  </li>
+                </ul>
+
+                <h4 className="my-3">Rocket Details</h4>
+                <ul className="list-group mb-4">
+                  <li className="list-group-item">
+                    Rocket Name: {rocket_name}
+                  </li>
+                  <li className="list-group-item">
+                    Rocket Type: {rocket_type}
+                  </li>
+                </ul>
+
+                <h4 className="my-3">Details</h4>
+                <ul className="list-group mb-4">
+                  <li className="list-group-item">{details}</li>
+                </ul>
+
+                <Link to="/" className="list-group-item btn-secondary">
+                  Back
+                </Link>
+              </>
             );
           }}
         </Query>
