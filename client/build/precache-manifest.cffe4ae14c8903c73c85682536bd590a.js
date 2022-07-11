0f15775f52ac91783b9edfc544176de92eabec23
xBasox <borko12329@gmail.com>
2022-07-11 19:00:30 +0300

    finishing touches

---
+++ client/build/precache-manifest.cffe4ae14c8903c73c85682536bd590a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a85913805d2d99df57b3",
+    "url": "/static/css/main.dd18e78e.chunk.css"
+  },
+  {
+    "revision": "a85913805d2d99df57b3",
+    "url": "/static/js/main.a8591380.chunk.js"
+  },
+  {
+    "revision": "4d7befa68b4edd895d07",
+    "url": "/static/css/1.110ac1ea.chunk.css"
+  },
+  {
+    "revision": "4d7befa68b4edd895d07",
+    "url": "/static/js/1.4d7befa6.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ade9ebdbefafa780cbab820c2bf9b9df",
+    "url": "/index.html"
+  }
+];
